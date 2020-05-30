78b4536fb4e176d10291ae9bdc971b085540a07c
Sanya <32676750+dear-s@users.noreply.github.com>
2020-05-29 21:31:27 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,126 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import plotly.express as px
+#
+# st.title("Hello World!")
+# st.markdown("## My first streamlit dashboard!")
+#
+
+# FILE PATH TO CSV FILE
+DATA_URL = (
+"Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used "
+"to analyse motor Vehicle Collisions in NYC 🗽💥🚗")
+
+# ONLY RERUN WHEN INPUTS HAVE CHANGED
+# handle update on the map
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
+    return data
+
+data = load_data(100000)
+original_data = data
+
+# WHERE MOST PEOPLE ARE INJURED
+# FOR COLLISIONS, OVERLAY THE DATA USING INJURIES
+# INTERACTIVITY!!! - ACTUAL WEB APPLICATION
+# NEW QUESTION
+st.header("Where are the most people injured in NYC?")
+# 19 is the max number of injuries - total=19
+injured_people = st.slider("Number of persons injured in Vehicle Collisions", 0, 19)
+# plot our data on map
+st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
+# any of them na drop entire row
+# we performed lowercase operation before
+# returning two cols so we can plot it on the map
+
+
+
+# how many collisions occured during a given time of day
+st.header("How many collisions occured during a given time of day?")
+# hardcode the hour or make it more interactive
+# make a dropdown
+hour = st.slider("Hour to look at", 0, 23)
+data = data[data['date/time'].dt.hour == hour]
+
+
+# plot filtered data on a 3d map
+st.markdown("Vehicle collisions between %i:00 and %i:00" % (hour, (hour + 1) % 24))
+
+# coords for initial_view_state
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+
+
+# pydeck-Deck GL???
+st.write(pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state={
+        'latitude': midpoint[0],
+        'longitude': midpoint[1],
+        'zoom': 11,
+        'pitch': 50,
+    },
+    layers = [
+        pdk.Layer(
+        "HexagonLayer",
+        data = data[['date/time', 'latitude', 'longitude']],
+        get_position = ['longitude', 'latitude'],
+        radius = 100,
+        extruded = True, # 3d
+        pickable = True,
+        elevation_scale = 4,
+        elevation_range = [0,1000],
+        ),
+    ],
+))
+
+# charts and histograms
+# how many collisions have happened in a 60 minute window [1 hr]
+# plot ly - histogram, bar chart
+# numpy - calculate the values for the histogram
+st.subheader("Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1)%24))
+# filtering the data
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour + 1))
+]
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
+st.write(fig)
+
+
+
+# top 5 most dangerous streets
+# injured classes - people of NYC
+# which street collision took place - data column [on_street_name]
+st.header("Top 5 dangerous streets by affected type")
+select = st.selectbox('Affected type of people', ['Pedestrians','Cyclists','Motorists'])
+
+# if statements on the basis of selection from dropdown - need data that is not filtered - made a copy
+if select == 'Pedestrians':
+    st.write(original_data.query('injured_pedestrians >= 1')[["on_street_name", "injured_pedestrians"]].sort_values(by=["injured_pedestrians"], ascending=False).dropna(how='any')[:5])
+
+elif select == 'Cyclists':
+    st.write(original_data.query('injured_cyclists >= 1')[["on_street_name", "injured_cyclists"]].sort_values(by=["injured_cyclists"], ascending=False).dropna(how='any')[:5])
+
+# elif select == 'Motorists':
+else:
+    st.write(original_data.query('injured_motorists >= 1')[["on_street_name", "injured_motorists"]].sort_values(by=["injured_motorists"], ascending=False).dropna(how='any')[:5])
+
+
+
+
+# showing the raw data [this will change as we update data constraint]
+if st.checkbox("Show Raw Data", False):
+    st.subheader('Raw Data')
+    st.write(data)
